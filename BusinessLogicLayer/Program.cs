e57b4915fe5b2c89e7842bab085bf0ac2286a030
Vanolivano <gerasim.v@list.ru>
2019-02-14 16:39:40 +0300

    add main class by BLL

---
+++ BusinessLogicLayer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BusinessLogicLayer
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+//            using (var uow = new UnitOfWork())
+//            {
+//                uow.Users.Create(new User{Login = "Vanolivano", Password = "123"});
+//                uow.Save();
+//                User user =  new User(uow.Users.ReadAll().FirstOrDefault());
+//                
+//            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
