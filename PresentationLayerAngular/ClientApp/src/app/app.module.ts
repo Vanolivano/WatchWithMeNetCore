62bceaa89ceffb5055ef95842e99763fd990f46d
Vanolivano <gerasim.v@list.ru>
2019-05-16 16:54:15 +0300

    jwt auth + angular views

--- PresentationLayerAngular/ClientApp/src/app/app.module.ts
+++ PresentationLayerAngular/ClientApp/src/app/app.module.ts
@@ -1,41 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import { HttpModule, XHRBackend } from '@angular/http';
+import { AuthenticateXHRBackend } from './authenticate-xhr.backend';
 
+import { routing } from './app.routing';
+
+/* App Root */
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import {LobbyComponent} from "./lobby/lobby.component";
-import {RoomComponent, SafePipe} from "./room/room.component";
+
+/* Account Imports */
+import { AccountModule }  from './account/account.module';
+/* Dashboard Imports */
+import { DashboardModule }  from './dashboard/dashboard.module';
+
+import { ConfigService } from './shared/utils/config.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    LobbyComponent,
-    RoomComponent,
-    SafePipe
+    HeaderComponent,
+    HomeComponent      
   ],
   imports: [
-    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    HttpClientModule,
+    AccountModule,
+    DashboardModule,
+    BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'lobby', component: LobbyComponent },
-      { path: 'room', component: RoomComponent },
-    ])
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [ConfigService, { 
+    provide: XHRBackend, 
+    useClass: AuthenticateXHRBackend
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
